6b383d4492c518b2743ba37b61a245f0eaf1e5fa
logsprr <corinthiaswwyy@gmail.com>
2020-12-02 17:16:02 -0300

    Adição do projeto e  correção do db por hora

---
+++ src/main/java/br/ueg/webflux/config/SecurityConfig.java
@@ -0,0 +1,66 @@
+package br.ueg.webflux.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.authentication.ReactiveAuthenticationManager;
+import org.springframework.security.authentication.UserDetailsRepositoryReactiveAuthenticationManager;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
+import org.springframework.security.crypto.factory.PasswordEncoderFactories;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+import br.ueg.webflux.services.UserServiceImpl;
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+
+	@Bean
+	public SecurityWebFilterChain securityWebFilterChain(ServerHttpSecurity http) {
+		return http
+				.csrf().disable()
+				.authorizeExchange()
+				.pathMatchers(HttpMethod.POST, "/**").hasRole("ADMIN")
+				.pathMatchers(HttpMethod.GET, "/**").hasRole("ADMIN")
+				.anyExchange().authenticated()
+				.and()
+					.formLogin()
+				.and()
+					.httpBasic()
+				.and()
+					.build();
+	}
+	@Bean
+	public PasswordEncoder passwordEncoder() {
+	    return PasswordEncoderFactories.createDelegatingPasswordEncoder();
+	}
+	
+//	@Bean
+//	public MapReactiveUserDetailsService userDetailsService() {
+//		
+//		PasswordEncoder passEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+//		UserDetails admin = User.withUsername("admin")
+//										.password(passEncoder.encode("admin"))
+//										.roles("ADMIN", "USER")
+//										.build();
+//		
+//		UserDetails user = User.withUsername("ricardo")
+//										.password(passEncoder.encode("ricardo"))
+//										.roles("USER")
+//										.build();
+//		
+//		return new MapReactiveUserDetailsService(admin, user);
+//	}
+	
+	@Bean
+	ReactiveAuthenticationManager authenticationManager(UserServiceImpl userService, PasswordEncoder passwordEncoder) {
+		PasswordEncoder passEncoder = PasswordEncoderFactories.createDelegatingPasswordEncoder();
+		UserDetailsRepositoryReactiveAuthenticationManager authenticationManager = new UserDetailsRepositoryReactiveAuthenticationManager(userService);
+		authenticationManager.setPasswordEncoder(passEncoder);
+	    return authenticationManager;
+	}
+}
---
+++ src/main/java/br/ueg/webflux/document/Roles.java
@@ -0,0 +1,13 @@
+package br.ueg.webflux.document;
+
+import org.springframework.security.core.GrantedAuthority;
+
+public class Roles implements GrantedAuthority {
+
+	@Override
+	public String getAuthority() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
